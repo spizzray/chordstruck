f1efecaadd7ef144c2e11cbec0c0383cee9cb0ce
ClaireOzzz <107027910+ClaireOzzz@users.noreply.github.com>
2022-07-17 14:40:41 +0800

    gemfile ruby updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # get 'home/index'
   root "home#index"
+
+  get 'page1', to: 'page1#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
