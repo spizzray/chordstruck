bfcc02c491cea9fa47d679635953992aa23e80db
ClaireOzzz <107027910+ClaireOzzz@users.noreply.github.com>
2022-07-23 16:15:31 +0800

    Audio player working
    npm install material-ui-audio-player

--- src/pages/ChordRec.jsx
+++ src/pages/ChordRec.jsx
@@ -1,21 +1,32 @@
 import React from "react";
 import Typography from '@material-ui/core/Typography';
-
-import { createMuiTheme, ThemeProvider } from '@material-ui/core';
-import AudioPlayer from 'material-ui-audio-player';
+import ReactDOM from "react-dom";
+import mp3 from "./Wonderwall.mp3";
 
 
 const ChordRec = () => {
-      return (
-        <div >
-            
-            <Typography  variant="h4"  style={{ color:"#4836d1", fontWeight: 'bold', m: '6% 3% 1% 5%' }} >
-              DOES IT WORK?
-          </Typography>
-            
-            {/* <Button>Hook</Button> */}
-        </div>
-          
-      );
-    }
+  return (
+    <div >
+      
+      <Typography  variant="h4"  style={{ color:"#000000", fontWeight: 'bold', m: '6% 3% 1% 5%' }} >
+         DOES IT WORK?
+      </Typography>
+
+      <AudioPlayer />
+
+    </div>
+  );
+}
+class AudioPlayer extends React.Component {
+  render() {
+    return (
+      <div >
+        <audio ref="audio_tag" src={mp3} controls />
+      </div>
+    );
+  }
+}
+
+
 export default ChordRec;
+
