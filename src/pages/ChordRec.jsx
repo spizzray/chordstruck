ed7e7a800f45eef4ee997bed76f7a2ae86428f56
ClaireOzzz <107027910+ClaireOzzz@users.noreply.github.com>
2022-07-23 17:29:54 +0800

    instructions pop up for chordrec page

--- src/pages/ChordRec.jsx
+++ src/pages/ChordRec.jsx
@@ -1,11 +1,29 @@
 import React from "react";
+import { useState } from "react";
+import ReactDOM from "react-dom";
+
 import Typography from '@material-ui/core/Typography';
 import Box from '@mui/material/Box';
-import ReactDOM from "react-dom";
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
 import mp3 from "./Wonderwall.mp3";
 
 
 const ChordRec = () => {
+  const [open, setOpen] = useState(true);
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <div >
       <Box className='songTitle'>
@@ -17,6 +35,30 @@ const ChordRec = () => {
       <Box className='playerBox'>
         <AudioPlayer />
       </Box>
+
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+        sx={{
+          maxWidth: 'xs',
+          '& .MuiDialog-paper': { borderRadius: 3, width: '60%', maxHeight: 435 }
+        }}>
+        <DialogTitle textAlign={'center'} variant="h5" color="#000000" component="div" sx={{ fontWeight: 'bold', pt: '5%', textTransform: "uppercase" }}>
+          INSTRUCTIONS
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText textAlign={'center'} >
+            Follow the chords
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions >
+          <Button className='okButton' sx={{borderRadius:3}} onClick={handleClose}>
+            OK
+          </Button>
+        </DialogActions>
+      </Dialog>
 
     </div>
   );
