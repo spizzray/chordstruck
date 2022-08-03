ba7a9c185a80615ddf0cbf566ed75e500f3d4ad0
ClaireOzzz <107027910+ClaireOzzz@users.noreply.github.com>
2022-08-03 21:57:13 +0800

    added more dummy data and chords

--- src/data.js
+++ src/data.js
@@ -5,17 +5,19 @@ import Aminor7 from './assets/Chords/Aminor7.png';
 import Amajor from './assets/Chords/Amajor.png';
 import Dmajor6 from './assets/Chords/Dmajor6.png';
 import Eminor from './assets/Chords/Eminor.png';
+import Cmajor from './assets/Chords/C-major.png';
 
 
 export default function ChillHop() {
 	// ALL CHORDS IN THE SONG
-	let types = ["A:min7", "A:maj", "D:maj6", "E:min" ]
+	let types = ['Ab:maj', 'F:min', 'Db:maj', 'Eb:maj', 'C:min' ]
 	// LINK CHORDS TO THE CHORD DIAGRAMS
 	const chordtype = {
-		"A:min7": Aminor7,
-		"A:maj": Amajor,
-		"D:maj6": Dmajor6,
-		"E:min": Eminor,
+		'Ab:maj': Aminor7,
+		'F:min': Amajor,
+		'Db:maj': Dmajor6,
+		'Eb:maj': Eminor,
+		'C:min': Cmajor, //temporary until i find the correct chord diagram
 	}
 
 	const [currentIndex, setCurrentIndex] = useState(0);
@@ -120,10 +122,28 @@ export default function ChillHop() {
 
 
 const MLdata = [
-	(0.0, 7.430385487528345, 'A:min7'),
-	(7.430385487528345, 11.377777777777778, 'A:maj'),
-	(11.377777777777778, 15.185850340136055, 'D:maj6'),
-	(15.185850340136055, 18.808163265306124, 'E:min'),
+ (0.0, 7.430385487528345, 'Ab:maj'),
+ (7.430385487528345, 11.377777777777778, 'F:min'),
+ (11.377777777777778, 15.185850340136055, 'Db:maj'),
+ (15.185850340136055, 18.808163265306124, 'Eb:maj'),
+ (18.808163265306124, 22.569795918367348, 'Ab:maj'),
+ (22.569795918367348, 26.610068027210886, 'F:min'),
+ (26.610068027210886, 29.72154195011338, 'Db:maj'),
+ (29.72154195011338, 30.65034013605442, 'Ab:maj'),
+ (30.65034013605442, 33.90113378684807, 'Eb:maj'),
+ (33.90113378684807, 37.802086167800454, 'Ab:maj'),
+ (37.802086167800454, 41.61015873015873, 'F:min'),
+ (41.61015873015873, 45.55755102040816, 'Db:maj'),
+ (45.55755102040816, 47.55446712018141, 'Ab:maj'),
+ (47.55446712018141, 49.13342403628118, 'Eb:maj'),
+ (49.13342403628118, 53.03437641723356, 'Ab:maj'),
+ (53.03437641723356, 56.70312925170068, 'F:min'),
+ (56.70312925170068, 60.46476190476191, 'Db:maj'),
+ (60.46476190476191, 61.06848072562359, 'Eb:min'),
+ (61.06848072562359, 63.25115646258504, 'Eb:maj'),
+ (63.25115646258504, 64.22639455782313, 'C:min'),
+ (64.22639455782313, 66.22331065759637, 'F:min'),
+ (66.22331065759637, 68.26666666666667, 'Db:maj'),
+ (68.26666666666667, 70.0778231292517, 'Ab:maj'),
 ]
-
 
